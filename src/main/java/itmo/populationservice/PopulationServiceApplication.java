2fd2e98053d336c426fd7100f1ebcb0cf88f3b17
kbushmelev <kostikbushmelev@gmail.com>
2025-10-31 08:01:36 +0300

    деплой в wildfly

--- src/main/java/itmo/populationservice/PopulationServiceApplication.java
+++ src/main/java/itmo/populationservice/PopulationServiceApplication.java
@@ -2,12 +2,14 @@ package itmo.populationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PopulationServiceApplication {
+public class PopulationServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PopulationServiceApplication.class, args);
@@ -25,5 +27,10 @@ public class PopulationServiceApplication {
                         .allowCredentials(false);
             }
         };
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PopulationServiceApplication.class);
     }
 }
