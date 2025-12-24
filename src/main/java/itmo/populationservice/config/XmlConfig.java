e27a694c93708a8ac20cbc113b3ada84c40f24ba
kbushmelev <kostikbushmelev@gmail.com>
2025-12-25 02:31:10 +0300

    create xsd file and generate classes

--- src/main/java/itmo/populationservice/config/XmlConfig.java
+++ src/main/java/itmo/populationservice/config/XmlConfig.java
@@ -1,20 +1,20 @@
-package itmo.populationservice.config;
-
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
-
-@Configuration
-public class XmlConfig {
-
-    @Bean
-    public MappingJackson2XmlHttpMessageConverter xmlHttpMessageConverter() {
-        XmlMapper xmlMapper = new XmlMapper();
-        xmlMapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        MappingJackson2XmlHttpMessageConverter converter = new MappingJackson2XmlHttpMessageConverter();
-        converter.setObjectMapper(xmlMapper);
-        return converter;
-    }
-}
+//package itmo.populationservice.config;
+//
+//import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+//
+//@Configuration
+//public class XmlConfig {
+//
+//    @Bean
+//    public MappingJackson2XmlHttpMessageConverter xmlHttpMessageConverter() {
+//        XmlMapper xmlMapper = new XmlMapper();
+//        xmlMapper.configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//
+//        MappingJackson2XmlHttpMessageConverter converter = new MappingJackson2XmlHttpMessageConverter();
+//        converter.setObjectMapper(xmlMapper);
+//        return converter;
+//    }
+//}
